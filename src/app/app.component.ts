9329030fb36e4983b42ec7007fcf4b1ce450fa36
emyengineer <emyengineer@gmail.com>
2019-06-12 10:51:52 +0200

    Binding to Custom Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  serverElements = [];
+export class AppComponent implements OnInit {
+  serverElements = [{ type: 'server', name: 'Testserver', content: 'First Server' }];
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    console.log(this.serverElements);
+  }
 
 }
